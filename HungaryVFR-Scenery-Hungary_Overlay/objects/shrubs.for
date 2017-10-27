7aab3f51795fd1877fe8e71429dc61f8836a2911
darealgege <gergely.debreceni@gmail.com>
2017-10-27 15:47:12 +0200

    2.0.0
    osm build: 2017.10.25

---
+++ HungaryVFR-Scenery-Hungary_Overlay/objects/shrubs.for
@@ -0,0 +1,17 @@
+A
+800
+FOREST
+
+TEXTURE shrubs.png
+SCALE_X 2048
+SCALE_Y 2048
+
+# Tree every 10 meters
+SPACING 25 25
+
+# Vary placement by 5 meters in any direction
+RANDOM 10 10
+
+TREE	20	499	500	500	256	100 5 10 2 1 shrub 1
+
+SKIP_SURFACE water
