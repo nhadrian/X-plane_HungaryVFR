95c6b71743cc30918abdb8eaa29f2b8aabb9243d
darealgege <gergely.debreceni@gmail.com>
2018-01-14 19:23:35 +0100

    2.0.1
    minor fixes

---
+++ HungaryVFR-Scenery-Hungary_Overlay/HungaryVFR/Forests/3Dgrass_dry.for
@@ -0,0 +1,38 @@
+I
+800
+FOREST
+
+CHOICE_PARAMS 50 0.75 5 0.25 0 0
+
+TEXTURE	3Dgrass_dry.dds
+NO_SHADOW
+
+LOD 500.0000
+
+SCALE_X 1024
+SCALE_Y 1024
+
+SPACING 2.0 2.0
+RANDOM 1.0 1.0
+
+
+
+#tree	   s	   t	   w	   y	offset	%	h min	h max	quads	type	name
+#-------------------------------------------------------------------------------------------
+TREE	0011	0816	0472	0209	0228	15	0.4	0.7	1	1	grass1
+TREE	0011	0621	0472	0186	0228	10	0.4	0.8	1	1	grass3
+TREE	0589	0621	0435	0173	0217	20	0.4	0.8	1	2	grass4
+TREE	0000	0000	0536	0417	0268	25	0.6	1.3	1	3	grass7
+TREE	0536	0000	0489	0428	0244	30	0.6	1.3	1	3	grass8
+
+SKIP_SURFACE water
+SKIP_SURFACE concrete
+SKIP_SURFACE asphalt
+SKIP_SURFACE gravel
+
+
+
+
+
+
+
