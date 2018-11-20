d2baef34ef14cb3676e538d461351f97be1e1a43
darealgege <gergely.debreceni@gmail.com>
2018-11-20 22:55:51 +0100

    1.0.5

---
+++ HungaryVFR-LHJK-Jakabszallas/for/UV_hemp_field.for
@@ -0,0 +1,29 @@
+A
+800
+FOREST
+
+TEXTURE 2.png
+
+LOD 25000
+
+SCALE_X 2048
+SCALE_Y 2048
+
+SPACING 1 1
+
+RANDOM 0 0
+
+#low-left coord tex size center percentage    ----height----
+#tree  s 	t 	w 	y 	offset occurrence    	min 	 max  	quads 	type 	name
+#---------------------------------------------------------------------------------------------------
+TREE	-27.7418	110.7	1137.41	1987.3	610.319	50	1	3	3	1	hemp1	
+TREE	1081.93	87.5821	1012.57	1692.25	508.599	50	1	3	3	1	hemp2	
+
+GROUP 1 100
+
+SKIP_SURFACE water
+
+
+# XSmartForests by Rhard. Version 1.0 alpha - Oct  5 2012
+# This program is free to use.
+# Feld_hanf
