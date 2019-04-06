9e5f120d94b4c39bf4ee6a9cd31d9fecb503dcdc
nhadrian <nhadrian@gmail.com>
2019-04-06 20:31:45 +0200

    1.5.4.6
    autohide HAVES and HAJAJ if plane itself is loaded

--- HungaryVFR-Library/plugins/SASLFree/data/modules/main.lua
+++ HungaryVFR-Library/plugins/SASLFree/data/modules/main.lua
@@ -4,6 +4,8 @@ size = { 1024 , 1024 }
 ----------------------------------------------------------------------------------
 
 hajon = createGlobalPropertyi("hungaryvfr/library/HAJON", 0.0)
+hajaj = createGlobalPropertyi("hungaryvfr/library/HAJAJ", 0.0)
+haves = createGlobalPropertyi("hungaryvfr/library/HAVES", 0.0)
 tailnum = globalPropertys("sim/aircraft/view/acf_tailnum")
 
 ----------------------------------------------------------------------------------
@@ -15,6 +17,19 @@ function update()
   else
     set(hajon, 0)
   end
+
+  if get(tailnum) == "HA-JAJ" then
+    set(hajaj, 1)
+  else
+    set(hajaj, 0)
+  end
+
+  if get(tailnum) == "HAVES" then
+    set(haves, 1)
+  else
+    set(haves, 0)
+  end
+  
   updateAll(components)
 end
 
